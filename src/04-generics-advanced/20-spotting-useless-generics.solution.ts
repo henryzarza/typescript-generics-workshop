1ccd4cd434e113105ea8594b752ccc01a53645b2
Henry Zarza <henryacz7@gmail.com>
2024-04-17 19:55:41 -0500

    Exercise 20

---
+++ src/04-generics-advanced/20-spotting-useless-generics.solution.ts
@@ -0,0 +1,22 @@
+import { expect, it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
+
+const returnBothOfWhatIPassIn = <T, U>(
+  params: {
+    a: T,
+    b: U
+  },
+): [T, U] => {
+  return [params.a, params.b];
+};
+
+it("Should return a tuple of the properties a and b", () => {
+  const result = returnBothOfWhatIPassIn({
+    a: "a",
+    b: 1,
+  });
+
+  expect(result).toEqual(["a", 1]);
+
+  type test1 = Expect<Equal<typeof result, [string, number]>>;
+});
